fd0cdf8c8518e7f827af78098b0525484628e205
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-09-13 09:33:08 +0300

    gevorg is over

--- gevorg.py
+++ gevorg.py
@@ -15,31 +15,45 @@
 #     for j in range(0, len(matrix[i]), 2):
 #         print(matrix[i][j] )
 
+# from random import randint
+#
+# m = 5
+# n = 5
+# matr = []
+# middle = 0
+# for i in range(m):
+#     l = []
+#     for j in range(n):
+#         num = randint(1,10)
+#         middle += num
+#         l.append(num)
+#     matr.append(l)
+# middle /= (m*n)
+# min_diff = float('inf')
+# s = 0
+# stolb = 0
+# for i in range(m):
+#     for j in range(n):
+#         diff = abs(middle - matr[i][j])
+#         if diff < min_diff:
+#             s = i
+#             stolb = j
+#             min_diff = diff
+#
+# print(matr)
+# print(middle)
+# print(s, stolb)
+
 from random import randint
 
-m = 5
-n = 5
+m = int(input())
 matr = []
-middle = 0
 for i in range(m):
     l = []
-    for j in range(n):
+    for j in range(m):
         num = randint(1,10)
-        middle += num
         l.append(num)
     matr.append(l)
-middle /= (m*n)
-min_diff = float('inf')
-s = 0
-stolb = 0
-for i in range(m):
-    for j in range(n):
-        diff = abs(middle - matr[i][j])
-        if diff < min_diff:
-            s = i
-            stolb = j
-            min_diff = diff
+
 
 print(matr)
-print(middle)
-print(s, stolb)
