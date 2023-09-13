a41176c04952f117068d391add6015f0e5aed0d9
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-09-13 09:32:44 +0300

    gevorg is over

--- 2.py
+++ 2.py
@@ -6,13 +6,13 @@
 #                 if not((x == (w or y)) or (w <= z) and (y <= w)):
 #                     print(x, y, z, w)
 #
+
 print("x y z w f")
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                f = int((x or y) and (not(y==z)) and not(w))
-                if f:
+                f = int(((x == (not(y))) <= (y and not(z))) or (z and not(w)))
+                if not f:
                     print(x, y, z, w)
-
 
