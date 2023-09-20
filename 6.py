591d3c30e4d997b34b25f058b9c2a0c32a596b87
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-09-20 09:51:25 +0300

    itertools

---
+++ 6.py
@@ -0,0 +1,65 @@
+import itertools
+
+# s = set()
+# word = "вава"
+# for w in itertools.permutations(word, r=3):
+#     s.add("".join(w))
+# print(len(s))
+# print(*s, sep="\n")
+# print(help(itertools.permutations))
+
+# s = set()
+# word = "абвгд"
+# for w in itertools.product(word, repeat=5):
+#     w = "".join(w)
+#     if w[0] != "а" and "аа" not in w and "бб" not in w and "вв" not in w and "гг" not in w and "дд" not in w:
+#         s.add(w)
+# print(len(s))
+# print(s)
+
+# s = set()
+# n = 0
+# word = "101101"
+# for w in itertools.permutations(word, 4):
+#     w = "".join(w)
+#     if "11" not in w and "00" not in w:
+#         s.add(w)
+#         n += 1
+# print(n)
+
+# s = set()
+# word = "росомаха"
+# for w in itertools.permutations(word, r=8):
+#     w = "".join(w)
+#
+#     if "11" not in w and "оо" not in w and "00" not in w:
+#         s.add(w)
+# print(len(s))
+# print(s)
+
+# s = set()
+# word = "андрей"
+# for w in itertools.product(word, repeat=7):
+#     w = "".join(w)
+#     if w[0] != "й" and w.count("й" ) == 1 and w.count("а") == 1:
+#         s.add(w)
+# print(len(s))
+# print(s)
+
+# s = set()
+# word = "тимофей"
+# for w in itertools.product(word, repeat=5):
+#     w = "".join(w)
+#     if w.count("т") >= 1 and w.count("й") <=1:
+#         s.add(w)
+# print(len(s))
+# print(s)
+
+s = set()
+word = "тимофей"
+for w in itertools.product(word, repeat=5):
+    w = "".join(w)
+    if w.count("т") >= 1 and w.count("й") <=1:
+        s.add(w)
+print(len(s))
+print(s)
