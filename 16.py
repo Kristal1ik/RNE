faa501b44692025f3343d507dfed1641435202a1
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-09-29 12:04:20 +0300

    16

--- 16.py
+++ 16.py
@@ -3,11 +3,34 @@ import sys
 sys.setrecursionlimit(1000000)
 
 
-def f(n):
-    if n > 2024:
-        return n
-    elif n <= 2024:
-        return n * f(n + 1)
+# def f(n):
+#     if n > 2024:
+#         return n
+#     elif n <= 2024:
+#         return n * f(n + 1)
+#
+#
+# print(f(2022) / f(2024))
 
+# def ff(n):
+#     if n < 11:
+#         return n
+#     elif n >= 11:
+#         return n + ff(n - 1)
+#
+#
+# print(ff(2024) - ff(2021))
 
-print(f(2022) / f(2024))
+# def f(n):
+#     if n < 3:
+#         return 1
+#     elif n > 2 and n % 2 != 0:
+#         return f(n - 1) + 3 * f(n - 2)
+#     if n > 2 and n % 2 == 0:
+#         summ = 0
+#         for i in range(1, n):
+#             summ += (f(i))
+#         return summ
+#
+#
+# print(f(28))
