ce5e1d85b283a2d5c2bc8feb8f58298eee7cb23e
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-10-13 12:49:45 +0300

    4 slice

--- 5.py
+++ 5.py
@@ -41,12 +41,21 @@ def f_three(n):
     return s
 
 
-lst = []
-for i in range(1, 1000):
-    n = f_three(i)
-    if i % 3 != 0:
-        n = n + f_three((i % 3) * 5)
-    r = int(n, 3)
-    if r > 146:
-        lst.append(i)
-print(min(lst))
+#
+# lst = []
+# for i in range(1, 1000):
+#     n = f_three(i)
+#     if i % 3 != 0:
+#         n = n + f_three((i % 3) * 5)
+#     r = int(n, 3)
+#     if r > 146:
+#         lst.append(i)
+# print(min(lst))
+
+for i in range(131, 256):
+    n = bin(i)[2:].rjust(8, "0")
+    n_new = n[:2] + n[-2:]
+    n_new = int(n_new, 2)
+    if n_new == 10:
+        print(i, n_new)
+        break
