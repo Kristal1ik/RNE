cb02f78eacd17b80e471e02234b50f6984155361
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-10-21 20:00:37 +0300

    hard

--- 2.py
+++ 2.py
@@ -7,12 +7,28 @@
 #                     print(x, y, z, w)
 #
 
-print("x y z w f")
+print("x y z w f1 f2")
+# for x in range(2):
+#     for y in range(2):
+#         for z in range(2):
+#             for w in range(2):
+#                 f = int(((x == (not(y))) <= (y and not(z))) or (z and not(w)))
+#                 if not f:
+#                     print(x, y, z, w)
 for x in range(2):
     for y in range(2):
         for z in range(2):
             for w in range(2):
-                f = int(((x == (not(y))) <= (y and not(z))) or (z and not(w)))
-                if not f:
-                    print(x, y, z, w)
+                F1 = int((w == x) and (y <= z))
+                F2 = int((w <= x) <= (y == z))
+                print(x, y, z, w, F1, F2)
+
+x y z w f1 f2
+
+
+1 0 1 1 1 0
+0 1 1 0 1 1
+1 0 1 0 0 0
+
+
 
