e4d2dd3ae4d164857e4d95a68ebd49a521208e23
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-11-24 11:44:17 +0300

    ab

--- 26.py
+++ 26.py
@@ -1,4 +1,3 @@
-
 # with open("26_1") as f:
 #     data = f.read().splitlines()
 # n = data[0]
@@ -98,26 +97,70 @@
 #         break
 # print(summa, max_)
 
-f = open("26_4.txt")
-s, n = list(map(int, f.readline().split()))
-data = []
-for i in range(n):
-    data.append(int(f.readline()))
-data.sort()
-print(s, data)
+# f = open("26_4.txt")
+# s, n = list(map(int, f.readline().split()))
+# data = []
+# for i in range(n):
+#     data.append(int(f.readline()))
+# data.sort()
+# print(s, data)
+#
+# summ_ = 0
+# count = 0
+# max_ = 0
+# for i in data:
+#     if summ_ + i <= s:
+#         count += 1
+#         summ_ += i
+#         max_ = i
+#     else:
+#         break
+# for i in range(n-1, 0, -1):
+#     if summ_ - max_ + data[i] <= s:
+#         max_ = data[i]
+#         break
+# print(count, max_)
+#
 
-summ_ = 0
-count = 0
-max_ = 0
-for i in data:
-    if summ_ + i <= s:
-        count += 1
-        summ_ += i
-        max_ = i
-    else:
-        break
-for i in range(n-1, 0, -1):
-    if summ_ - max_ + data[i] <= s:
-        max_ = data[i]
-        break
-print(count, max_)
+# with open("26_2024.txt") as f:
+#     data = f.read().splitlines()
+# n = data[0]
+# data = data[1:]
+# lst = []
+# for s in data:
+#     start, end = list(map(int, s.split()))
+#     lst.append([end, start])
+# lst.sort()
+# zal = 0
+# correct = []
+# break_ = []
+# for tusa in lst:
+#     if tusa[1] >= zal:
+#         correct.append(tusa)
+#         zal = tusa[0]
+#         break_.append(tusa[1] - zal)
+# print(len(correct))
+# print(correct)
+# mx = 0
+# for i in correct:
+#     mxx = abs(i[0] - i[1])
+#     if mx < mxx:
+#         mx = mxx
+# print(mx)
+
+f = open('inf_26_04_21_26.txt')
+data = f.readlines()
+n = list(map(int, data))
+m = 0
+c = 0
+ns = set(n)
+for i in range(1, len(n) - 1):
+    for j in range(i + 1, len(n)):
+        if (n[i] + n[j]) % 2 == 0:
+            summ = n[i] + n[j]
+            if summ in ns:
+                c += 1
+                if summ > m:
+                    m = summ
+
+print(c, m)
