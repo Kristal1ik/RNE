3298742035d3017114d39f8eb142e400251f9387
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-12-07 19:01:09 +0300

    dict (?)

--- 9.py
+++ 9.py
@@ -1,8 +1,14 @@
-with open('9.txt') as f:
-    data = f.read().splitlines()
-n = 0
-for i in data:
-    i = list(map(int, i.split("\t")))
-    if len(i) == len(set(i)) and (((max(i) + min(i)) / 2) < (sum(i) / len(i))):
-        n += 1
-print(n)
+# with open('9.txt') as f:
+#     data = f.read().splitlines()
+# n = 0
+# for i in data:
+#     i = list(map(int, i.split("\t")))
+#     if len(i) == len(set(i)) and (((max(i) + min(i)) / 2) < (sum(i) / len(i))):
+#         n += 1
+# print(n)
+
+data = [1,2,2]
+sums = {}
+for element in data:
+    sums[element] = data.count(element) * element
+print(sums)
