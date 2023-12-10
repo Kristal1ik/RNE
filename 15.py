3b40e169d266ef3e22ef5008282a6877f3504491
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-12-10 19:06:38 +0300

    interesting 15 before the егкр..

--- 15.py
+++ 15.py
@@ -1,79 +1,24 @@
-# from turtle import *
-#
-# speed(0)
-# forward(100)
-# right(90)
-# forward(120)
-# right(90)
-# forward(100)
-# right(90)
-# forward(120)
-# right(45)
-# forward(71)
-# right(90)
-# forward(71)
-# right(45)
-# up()
-# forward(30)
-# right(90)
-# forward(7)
-# down()
-# forward(33)
-# left(90)
-# forward(33)
-# left(90)
-# forward(33)
-# left(90)
-# forward(33)
-# left(90)
-# down()
-# up()
-# forward(53)
-# down()
-# forward(33)
-# left(90)
-# forward(33)
-# left(90)
-# forward(33)
-# left(90)
-# forward(33)
-# left(90)
-# left(90)
-# up()
-#
-# forward(50)
-# right(90)
-# forward(2)
-# down()
-# left(90)
-# forward(40)
-# left(90)
-# forward(30)
-# left(90)
-# forward(40)
-# left(90)
-# forward(15)
-# left(90)
-# forward(40)
-# right(180)
-# forward(40)
-# left(90)
-# forward(15)
-# done()
-
-# k = 10 # Масштаб
-# up()
-# for x in range(-10, 10):
-#     for y in range(-10, 10):
-#         goto(x*k, y*k)
-#         dot(5)
-# down()
 
 
-for A in range(300):
-    n = 0
-    for x in range(300):
-        if ((x & 42 != 0) or (x & 13 != 0)) <= ((x & 30 == 0) <= (x & A != 0)):
-            n += 1
-    if n == 300:
-        print(A)
+# for A in range(300):
+#     n = 0
+#     for x in range(300):
+#         if ((x & 42 != 0) or (x & 13 != 0)) <= ((x & 30 == 0) <= (x & A != 0)):
+#             n += 1
+#     if n == 300:
+#         print(A)
+
+
+p = [i for i in range(3, 39)]
+q = [i for i in range(21, 58)]
+A = []
+mx_len = 0
+for a1 in range(100):
+    for a2 in range(a1, 100):
+        n = 0
+        for x in range(100):
+            if ((x in q) <= (x in p)) <= (not (a1 <= x <= a2)):
+                n += 1
+        if n == 100:
+            mx_len = max(mx_len, a2-a1+1)
+print(mx_len)
