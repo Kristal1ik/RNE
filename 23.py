8a3eda9679466a0de9c0877297a442145aa6d3c6
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-12-15 12:34:52 +0300

    Чайкин(с простыми числами)

--- 23.py
+++ 23.py
@@ -22,13 +22,45 @@
 # print(f(78, 4, [2]))
 
 
+# def f(x, y, his):
+#     if x == y and 14 not in his:
+#         return 1
+#     if x > y:
+#         return 0
+#     return f(x + 1, y, his + [x + 1]) + f(x * 2, y, his + [x * 2]) + f(x ** 2, y, his + [x ** 2])
+#
+#
+# print(f(3, 25, [3]))
+
+
+# def f(x, y, his):
+#     if x == y and 15 not in his and 14 in his:
+#         return 1
+#     if x > y:
+#         return 0
+#     return f(x + 1, y, his + [x + 1]) + f(x + 3, y, his + [x + 3]) + f(x * 3, y, his + [x * 3])
+#
+#
+# print(f(7, 20, [7]))
+
+def eval_count(m):
+    n = 0
+    for i in m:
+        count = 0
+        for j in range(1, i):
+            if i % j == 0:
+                count += 1
+        if count == 1:
+            n += 1
+    return n
+
+
 def f(x, y, his):
-    if x == y and 14 not in his:
+    if x == y and eval_count(his) == 3:
         return 1
     if x > y:
         return 0
-    return f(x + 1, y, his + [x + 1]) + f(x * 2, y, his + [x * 2]) + f(x ** 2, y, his + [x ** 2])
+    return f(x + 1, y, his + [x + 1]) + f(x + 3, y, his + [x + 3]) + f(x * 2, y, his + [x * 2])
 
 
-print(f(3, 25, [3]))
-
+print(f(1, 35, [1]))
