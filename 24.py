b02060d243f920d4f56be0fb72227a1c0ce5c944
Kristal1ik <90793805+Kristal1ik@users.noreply.github.com>
2023-12-20 10:57:33 +0300

    после повторяющихся букв

--- 24.py
+++ 24.py
@@ -28,15 +28,42 @@
 #         now = 1
 #         count = 1
 # print(n)
-with open("24") as file:
-    data = file.readline()
-n = 1
-max_n = 0
-for i in range(1, len(data)):
-    if data[i] != data[i - 1]:
-        n += 1
-    else:
-        max_n = max(n, max_n)
-        n = 1
-max_n = max(n, max_n)
-print(max_n)
+
+# with open("24") as file:
+#     data = file.readline()
+# n = 1
+# max_n = 0
+# for i in range(1, len(data)):
+#     if data[i] != data[i - 1]:
+#         n += 1
+#     else:
+#         max_n = max(n, max_n)
+#         n = 1
+# max_n = max(n, max_n)
+# print(max_n)
+
+# with open("24_1") as file:
+#     data = file.readline()
+# n = 1
+# max_n = 0
+# for i in range(0, len(data) - 2, 3):
+#     if data[i] + data[i+1] + data[i+2] == "LDR":
+#         print(data[i], data[i + 1], data[i + 2])
+#
+#         n += 1
+#     else:
+#         max_n = max(n, max_n)
+#         n = 1
+# max_n = max(n, max_n)
+# print(max_n)
+s = "abcdefghiujklmnopqrstuvwxyz"
+with open("24_2.txt") as file:
+    data = file.readline().lower()
+d = {}
+for i in range(27):
+    d[s[i]] = 0
+for i in range(1, len(data) - 2):
+    if data[i] == data[i + 1]:
+        d[data[i + 2]] += 1
+print(d)
+print(max(d.values()))
