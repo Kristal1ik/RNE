a1030cccd8d6b31bafceacfd09fae956487905fa
Kristallik <90793805+Kristal1ik@users.noreply.github.com>
2024-05-15 17:25:27 +0300

    пробник 27 апреля 2 вариант

--- 9.py
+++ 9.py
@@ -13,28 +13,60 @@
 #     sums[element] = data.count(element) * element
 # print(sums)
 
-with open("9.csv") as f:
+# with open("9.csv") as f:
+#     data = f.read().splitlines()
+# n = 0
+# for i in data:
+#     line = list(map(int, i.split(";")))
+#     d = dict()
+#     for j in line:
+#         if j not in d.keys():
+#             d[j] = 1
+#         else:
+#             d[j] += 1
+#     f3 = False
+#     f1_count = 0
+#     f3_num = 0
+#     f1_sum = 0
+#     for k, v in d.items():
+#         if v == 3:
+#             f3 = True
+#             f3_num = k
+#         elif v == 1:
+#             f1_count += 1
+#             f1_sum += k
+#     if f3 and f1_count == 4 and f1_sum / 4 < f3_num:
+#         n += 1
+# print(n)
+
+with open("9_4.txt", encoding="utf-8") as f:
     data = f.read().splitlines()
 n = 0
+
 for i in data:
     line = list(map(int, i.split(";")))
     d = dict()
     for j in line:
-        if j not in d.keys():
+        if j not in d:
             d[j] = 1
         else:
             d[j] += 1
-    f3 = False
+    f2 = False
     f1_count = 0
-    f3_num = 0
-    f1_sum = 0
+    lst1 = []
+    f2_num = 0
     for k, v in d.items():
-        if v == 3:
-            f3 = True
-            f3_num = k
+        if v == 2:
+            f2 = True
+            f2_num = k
         elif v == 1:
             f1_count += 1
-            f1_sum += k
-    if f3 and f1_count == 4 and f1_sum / 4 < f3_num:
-        n += 1
+            lst1.append(k)
+    if len(lst1) == 5 and f2:
+        proizv = 1
+        lst1 = sorted(lst1)[:3]
+        for j in lst1:
+            proizv *= j
+        if proizv > f2_num ** 2:
+            n += 1
 print(n)
