ff7ccaaaa8db5b76d824f012d203d4edf70dbe1f
Kristal1ik <korobovtsevaolga@yandex.ru>
2024-06-03 19:22:40 +0300

    три кучи

--- 19-21 БУ.py
+++ 19-21 БУ.py
@@ -21,14 +21,34 @@ from functools import lru_cache  # импортируем кэширование
 #             n += 1
 # print(n)
 
+# @lru_cache(None)
+# def f(a):
+#     if a >= 65:
+#         return 0  # условие выигрыша
+#     if a * 3 > 109:
+#         moves = [f(a + 7), f(a + 5)]
+#     else:
+#         moves = [f(a + 7), f(a * 3), f(a + 5)]
+#     win_petya = [i for i in moves if i <= 0]
+#     if win_petya:
+#         return -max(win_petya) + 1
+#     else:
+#         return -max(moves)
+#
+#
+# n = 0
+# for i in range(1, 40):
+#     if f(i) == 2:
+#         print(i)
+# print(n)
+
 @lru_cache(None)
-def f(a):
-    if a >= 65:
+def f(a, b, c):
+    if a + b + c >= 300:
         return 0  # условие выигрыша
-    if a * 3 > 109:
-        moves = [f(a + 7), f(a + 5)]
-    else:
-        moves = [f(a + 7), f(a * 3), f(a + 5)]
+
+    moves = [f(a+b+c, b, c), f(a, b+a+c, c), f(a, b, c+a+b),
+             f(a*3, b, c), f(a, b*3, c), f(a, b, c*3)]
     win_petya = [i for i in moves if i <= 0]
     if win_petya:
         return -max(win_petya) + 1
@@ -37,7 +57,10 @@ def f(a):
 
 
 n = 0
-for i in range(1, 40):
-    if f(i) == 2:
-        print(i)
+lst = []
+for i in range(1, 101):
+    if f(45, i, i + 25) == 3 and not (f(45, i, i + 25) == 2) and not (f(45, i, i + 25) == 1):
+        lst.append(i)
+        n += 1
 print(n)
+print(lst)
