59abf5fa47ce1c3ccfbac45a3445f9ca027adf2f
Kristal1ik <korobovtsevaolga@yandex.ru>
2024-06-04 01:11:24 +0300

    простые на одну кучу

--- 19-21 БУ.py
+++ 19-21 БУ.py
@@ -42,14 +42,38 @@ from functools import lru_cache  # импортируем кэширование
 #         print(i)
 # print(n)
 
-@lru_cache(None)
-def f(a, b, c):
-    if a + b + c >= 150:
-        return 0  # условие выигрыша
+# @lru_cache(None)
+# def f(a, b, c):
+#     if a + b + c >= 150:
+#         return 0  # условие выигрыша
+#
+#     moves = [f(a+16, b, c), f(a, b+16, c), f(a, b, c+16),
+#              f(a*2, b, c), f(a, b*2, c), f(a, b, c*2),
+#              f(a+32, b, c), f(a, b+32, c), f(a, b, c+32)]
+#     win_petya = [i for i in moves if i <= 0]
+#     if win_petya:
+#         return -max(win_petya) + 1
+#     else:
+#         return -max(moves)
+#
+#
+# n = 0
+# lst = []
+# for i in range(1, 101):
+#     if f(6, 2 * i, i * 3) == -2 and not(f(6, 2 * i, i * 3) == -1):
+#         lst.append(i)
+#         n += 1
+# print(n)
+# print(lst)
 
-    moves = [f(a+16, b, c), f(a, b+16, c), f(a, b, c+16),
-             f(a*2, b, c), f(a, b*2, c), f(a, b, c*2),
-             f(a+32, b, c), f(a, b+32, c), f(a, b, c+32)]
+@lru_cache(None)
+def f(a):
+    if a >= 100:
+        return 0  # условие выигрыша
+    if a * 4 > 120:
+        moves = [f(a+3)]
+    else:
+        moves = [f(a + 3), f(a * 4)]
     win_petya = [i for i in moves if i <= 0]
     if win_petya:
         return -max(win_petya) + 1
@@ -59,9 +83,9 @@ def f(a, b, c):
 
 n = 0
 lst = []
-for i in range(1, 101):
-    if f(6, 2 * i, i * 3) == -2 and not(f(6, 2 * i, i * 3) == -1):
+for i in range(1, 61):
+    if f(i) == 3 and not(f(i) == 1) and not(f(i) == 2):
         lst.append(i)
         n += 1
-print(n)
+# print(n)
 print(lst)
