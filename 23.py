97c1ac95c0f30126fa320c7794854929962a2e47
Kristal1ik <korobovtsevaolga@yandex.ru>
2024-06-06 16:14:50 +0300

    егкр 2 var

--- 23.py
+++ 23.py
@@ -109,14 +109,31 @@
 #
 # print(f(41, 5, []))
 
-def f(n, t, his):
-    if n == t and 25 not in his:
-        return 1
-    if n > 115:
-        return 0
-    return f(n + 3, t, his + [n + 3]) + f(n * 2, t, his + [n * 2]) + f(n * 5, t, his + [n * 5])
+# def f(n, t, his):
+#     if n == t and 25 not in his:
+#         return 1
+#     if n > 115:
+#         return 0
+#     return f(n + 3, t, his + [n + 3]) + f(n * 2, t, his + [n * 2]) + f(n * 5, t, his + [n * 5])
+#
+#
+# print(f(5, 115, []))
+
+# def not_five(lst):
+#     for i in lst:
+#         if i % 5 == 0:
+#             return False
+#     return True
 
 
-print(f(5, 115, []))
+# def f(start, finish, his):
+#     if start == finish and not_five(his):
+#         return 1
+#     if start > finish:
+#         return 0
+#     return f(start + 4, finish, his + [start + 4]) + f(start + 5, finish, his + [start + 5])
+#
+# print(f(31, 60, []))
 
 
+def f()
